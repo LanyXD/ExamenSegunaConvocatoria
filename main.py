bdd471c02e941f82e27b836c712f85e4c6f3ef54
Miguel Matul <miguemat4@gmail.com>
2022-05-10 09:19:19 -0600

    El programa fue actualizado
    Ahora tiene un programa basado en la verificación de una contraseña

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
 print("Andy Isaac Monzón López - 1530522")
+
+key = "landivar2022"
+password = input("Ingrese la contraseña: ")
+if key == password.lower():
+    print("La contaseña es correcta")
+else:
+    print("La contraseña es incorrecta")
